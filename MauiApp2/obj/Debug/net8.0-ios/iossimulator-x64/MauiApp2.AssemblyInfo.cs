d12ae69c39ee3a09e9b47c457532dafd1880943c
Sorosberenc <neisermate@gmail.com>
2025-03-08 20:34:40 +0100

    ZH hozzaadas es kilistazas
    txt-ben mentes

--- MauiApp2/obj/Debug/net8.0-ios/iossimulator-x64/MauiApp2.AssemblyInfo.cs
+++ MauiApp2/obj/Debug/net8.0-ios/iossimulator-x64/MauiApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b2aa58b47accd4cfaf957ab163256dd4644ca511")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
